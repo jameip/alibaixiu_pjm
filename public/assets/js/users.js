0371c819601caa5e05b20505ad108bc76b03fa56
pjm <pjm@163.com>
2020-03-15 16:37:03 +0800

    完成修改功能

--- public/assets/js/users.js
+++ public/assets/js/users.js
@@ -5,7 +5,7 @@ $.ajax({
     url: '/users',
     success: function (res) {
         getAry = res
-        console.log(getAry);
+        // console.log(getAry);
         render()
     }
 });
@@ -41,31 +41,37 @@ $('#btnAdd').on('click', function () {
         url: '/users',
         data: data,
         success: function (res) {
-            getAry.push(res)
+            getAry.unshift(res)
             render();
             // 将表单数据清空
-            $('input[name="email"]').val('');
-            $('input[name="nickName"]').val('');
-            $('input[name="password"]').val('');
-            $('#status0').prop('checked', false);
-            $('#status1').prop('checked', false);
-            $('#admin').prop('checked', false);
-            $('#normal').prop('checked', false);
-            $('#hidden').val('')
-            $('#pic').attr('src', '../assets/img/default.png')
+            clear()
         }
     })
 })
-
+// 清空表单数据
+function clear(){
+    $('input[name="email"]').prop('disabled',false).val('');
+    $('input[name="nickName"]').val('');
+    $('input[name="password"]').prop('disabled',false).val('');
+    $('#status0').prop('checked', false);
+    $('#status1').prop('checked', false);
+    $('#admin').prop('checked', false);
+    $('#normal').prop('checked', false);
+    $('#hidden').val('')
+    $('#pic').attr('src', '../assets/img/default.png')
+}
 // 点击编辑按钮获取信息显示到左边信息栏
 // 英文按钮是后面添加的所以需要用到事件委托
+var usrId
 $('tbody').on('click', '.edit', function () {
+    usrId=$(this).attr('data-id')
     $('h2').html('编辑用户');
     let tr = $(this).parents('tr');
     $('#pic').attr('src', tr.find('img').attr('src'))
     $('#hidden').val(tr.find('img').attr('src'));
-    $('input[name="email"]').val(tr.children().eq(2).text());
+    $('input[name="email"]').prop('disabled',true).val(tr.children().eq(2).text());
     $('input[name="nickName"]').val(tr.children().eq(3).text());
+    $('input[name="password"]').prop('disabled',true);
     if(tr.children().eq(4).text()=='激活'){
         $('#status1').prop('checked',true);
     }else{
@@ -78,4 +84,28 @@ $('tbody').on('click', '.edit', function () {
     }
     $('#btnAdd').hide()
     $('#btnedit').show()
+    // getAry.find(function(item){
+    //     // console.log(item);
+        
+    // })
+    // console.log();    
+})
+// 修改用户信息功能
+$('#btnedit').on('click',function(){
+    let data=$('form').serialize();
+    $.ajax({
+        type:'PUT',
+        url:'/users/'+usrId,
+        data:data,
+        success:function(res){
+            // console.log(res);
+            let index=getAry.findIndex(itme=>itme._id==res._id)
+            getAry[index]=res
+            render();
+            clear();
+            $('h2').text('添加新用户');
+            $('#btnAdd').show();
+            $('#btnedit').hide();
+        }
+    })
 })
