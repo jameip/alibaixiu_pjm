5d4aea1048b5f28b24512580569737f0147ad2af
pjm <pjm@163.com>
2020-03-18 17:16:55 +0800

    完成了文章的添加编辑修改功能

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -37,3 +37,56 @@ $('#btnAdd').on('click',function(){
         }
     })
 })
+// 封装函数获取跳转携带的id值
+function getUserId(id){
+    let parmas=location.search.substr(1).split('&')
+    for(i=0;i<parmas.length;i++){
+        let temp=parmas[i].split('=')
+        if(temp[0]==id){
+            return temp[1]
+        }
+    }
+    return -1
+}
+let id=getUserId('id');
+if(id!=-1){
+    $.ajax({
+        type:'get',
+        url:'/posts/'+id,
+        success:function(res){
+            console.log(res);
+            $('h1').html('编辑文章')
+            $('#title').val(res.title);
+            $('#content').val(res.content);
+            $('#created').val(res.createAt.substr(0,16));
+            $('#btnEdit').show();
+            $('#btnAdd').hide();
+            $('.thumbnail').show().attr('src',res.thumbnail);
+            $('#hidden').val(res.thumbnail);
+            $('#category option').each(function(index,item){
+                if($(item).attr('value')==res.category._id){
+                    $(item).prop('selected',true);
+                }
+            })
+            $('#status option').each(function(index,item){
+                if($(item).attr('value')==res.state){
+                    $(item).prop('selected',true);
+                }
+            })
+        }
+    })
+}
+$('#btnEdit').on('click',function(){
+    let data=$('form').serialize();
+    $.ajax({
+        type:'put',
+        url:'/posts/'+id,
+        data:data,
+        success:function(){
+            location.href = 'posts.html';
+        }
+    })
+})
+
+
+
--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,27 +1,77 @@
-// 获取文章数据
-$.ajax({
-    type: 'get',
-    url: '/posts',
-    data: {
-        page: 1
-    },
-    success: function (res) {
-        console.log(res);
-
-        let html = template('ptpml', { data: res.records })
-        $('tbody').html(html)
-    }
-})
-// 时间处理格式
-// function formateDate(date) {
-// 	// 将日期时间字符串转换成日期对象
-// 	date = new Date(date);
-// 	return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
-// }
+var cid = $('#allfl').val();
+var state = $('#category').val();
+// 获取文章数据,英文都是查询文章列表请求，所以将请求封装成了一个函数
+render(cid,state)
+// 封装文章请求函数 es6新增的参数默认值page=1，如果不传page，默认就是1
+function render(cid,state,page=1){
+    $.ajax({
+        type: 'get',
+        url: '/posts',
+        data: {
+            page: page,
+            category: cid,
+            state: state
+        },
+        success: function (res) {
+            // console.log(res);
+            let html = template('ptpml', { data: res.records })
+            $('tbody').html(html);
+            let pagehtml = template('pagetpl', res)
+            $('#pageall').html(pagehtml)
+        }
+    })
+}
 //时间格式处理
 function format(str) {
     let iota1 = str.split(".")[0];
     let iota2 = iota1.split("T");
     let newTime = iota2.join(" ");
     return newTime;
+};
+// 分页功能
+var currentpage=1
+function changepage(page) {
+    currentpage=page
+    // console.log(currentpage);
+    render(cid,state,page)
 }
+// 所有分类数据
+$.ajax({
+    type: 'get',
+    url: '/categories',
+    success: function (res) {
+        let html = template('ftmp', { data: res })
+        // console.log(html);
+        $('#allfl').append(html)
+    }
+})
+// 筛选功能
+$('#search').on('click',function(){
+    cid = $('#allfl').val();
+    state = $('#category').val();
+    render(cid,state)
+})
+// 删除功能
+$('tbody').on('click','.delta',function(){
+    // alert(1)
+    let id=$(this).attr('data-id');
+   if(confirm('确认删除吗')){
+    $.ajax({
+        type:'delete',
+        url:'/posts/'+id,
+        success:function(res){
+            // 页面跳转功能优化
+           if($('tbody tr').length==1) {
+            if(currentpage==1){
+                render(cid, state, currentpage);
+            }else{
+                render(cid, state, --currentpage)
+            }
+           }else{
+            render(cid,state,currentpage)
+           }
+            
+        }
+    })
+   }
+})
