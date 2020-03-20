38a2dbc139f5fdc47f18e6b10b0c17358ac048ee
pjm <pjm@163.com>
2020-03-21 00:42:04 +0800

    完成相关链接跳转点赞和搜索功能

--- public/assets/js/publick.js
+++ public/assets/js/publick.js
@@ -1,13 +1,13 @@
 // 随机推荐
 $.ajax({
-    type: 'get',
-    url: '/posts/random',
-    success: function (res) {
-        // console.log(res);
-        let tpl=`
+  type: 'get',
+  url: '/posts/random',
+  success: function (res) {
+    // console.log(res);
+    let tpl = `
         {{each data}}
          <li>
-        <a href="javascript:;">
+        <a href="detail.html?id={{@$value._id}}">
           <p class="title">{{$value.title.substr(0,23)}}</p>
           <p class="reading">阅读({{$value.meta.views}})</p>
           <div class="pic">
@@ -17,23 +17,29 @@ $.ajax({
       </li>
       {{/each}}
       `
-        let html=template.render(tpl,{data:res})
-        $('.random').html(html)
-    }
+    let html = template.render(tpl, { data: res })
+    $('.random').html(html)
+  }
 })
 // 导航栏分类显示
 $.ajax({
-    type:'get',
-    url:'/categories',
-    success:function(res){
-        console.log(res);
-        let tpl=
-        `
+  type: 'get',
+  url: '/categories',
+  success: function (res) {
+    // console.log(res);
+    let tpl =
+      `
         {{each data}}
         <li><a href="list.html?id={{@$value._id}}"><i class="fa {{$value.className}}"></i>{{$value.title}}</a></li>
         {{/each}}
         `
-        let html=template.render(tpl,{data:res})
-        $('.nav_data').html(html)
-    }
+    let html = template.render(tpl, { data: res })
+    $('.nav_data').html(html)
+  }
+})
+// 搜索功能
+$('.search form').on('submit', function () {
+  // 获取到用户输入的值
+ location.href="/search.html?key="+$(this).find('.keys').val()
+  return false;
 })
